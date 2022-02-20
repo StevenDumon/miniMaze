b8cae5d69c10897d1f59f2bea34be92af4bf97ee
StevenDumon <100061638+StevenDumon@users.noreply.github.com>
2022-02-20 07:38:00 +0100

    First files, added via upload

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE HTML>
+<!--
+	Hielo by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Production Planning</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+    <?php include("scripts/menu.php"); ?>
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<div class="logo"><a href="index.php">Hielo <span>by TEMPLATED</span></a></div>
+				<a href="#menu">Menu</a>
+			</header>
+
+    <?php writeMenu(); ?>
+
+		<!-- Banner -->
+			<section class="banner full">
+				<article>
+					<img src="images/slide01.jpg" alt="" />
+					<div class="inner">
+						<header>
+							<p>A free responsive web site template by <a href="https://templated.co">TEMPLATED</a></p>
+							<h2>Production Planning</h2>
+						</header>
+					</div>
+				</article>
+				<!-- <article>
+					<img src="images/slide02.jpg" alt="" />
+					<div class="inner">
+						<header>
+							<p>Lorem ipsum dolor sit amet nullam feugiat</p>
+							<h2>Magna etiam</h2>
+						</header>
+					</div>
+				</article> -->
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style2">
+				<div class="inner">
+					<div class="grid-style">
+
+						<div> <!-- First box in grid -->
+							<div class="box">
+								<!-- <div class="image fit">
+									<img src="images/pic02.jpg" alt="" />
+								</div> -->
+								<div class="content">
+									<header class="align-center">
+										<p>View part information</p>
+										<h2>Parts</h2>
+									</header>
+									<p>Explore part information, part structures and history.</p>
+									<footer class="align-center">
+										<a href="selectPart.html" class="button alt">Parts</a>
+									</footer>
+								</div>
+							</div>
+						</div> <!-- End of first box -->
+
+						<div>
+							<div class="box">
+								<!-- <div class="image fit">
+									<img src="images/pic03.jpg" alt="" />
+								</div> -->
+								<div class="content">
+									<header class="align-center">
+										<p>Production planning</p>
+										<h2>Planning</h2>
+									</header>
+									<p>Use the planning tool to follow production orders, resources and machine usage.</p>
+									<footer class="align-center">
+										<a href="planning.php" class="button alt">Planning</a>
+									</footer>
+								</div>
+							</div>
+						</div>
+
+            <div> <!-- Third box, starts a new row in grid depending on window size -->
+							<div class="box">
+								<!-- <div class="image fit">
+									<img src="images/pic02.jpg" alt="" />
+								</div> -->
+								<div class="content">
+									<header class="align-center">
+										<p>Import XML file</p>
+										<h2>XML</h2>
+									</header>
+									<p>Import XML structure files to make PLM parts available for production</p>
+									<footer class="align-center">
+										<a href="selectXML.php" class="button alt">Import XML</a>
+									</footer>
+								</div>
+							</div>
+						</div> <!-- End of third box : import XML -->
+
+            <div> <!-- Fourth box in grid : database setup -->
+							<div class="box">
+								<!-- <div class="image fit">
+									<img src="images/pic02.jpg" alt="" />
+								</div> -->
+								<div class="content">
+									<header class="align-center">
+										<p>Database setup and check</p>
+										<h2>Database</h2>
+									</header>
+									<p>Tool for database setup and verification.</p>
+									<footer class="align-center">
+										<a href="scripts/database_setup.php" class="button alt">Database</a>
+									</footer>
+								</div>
+							</div>
+						</div> <!-- End of fourth box -->
+
+
+					</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+					</ul>
+				</div>
+				<div class="copyright">
+					&copy; Untitled. All rights reserved.
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
