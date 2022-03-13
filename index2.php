e1684330d1148193011e9c326f9b5481c690bca1
StevenDumon <steven.dumon@gmail.com>
2022-03-13 16:41:45 +0100

    Test AutoComplete

--- index2.php
+++ index2.php
@@ -1,68 +1,37 @@
 <!DOCTYPE HTML>
-<!--
-	Hielo by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
--->
 <html>
 	<head>
-		<title>miniMaze</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-    <?php include("scripts/menu.php"); ?>
+		<title>miniMaze test autocomplete</title>
 		<?php
 			include("scripts/database_connection.php");
 			$conn = new mysqli($servername, $username, $password, $dbname);
 		?>
-
 	</head>
-	<body class="subpage">
+	<body>
+		<?php
+			$query = "SELECT DISTINCT Number FROM XML_demo.Parts";
+			$result = $conn->query($query);
+			echo "<br>Option list contains " . $result->num_rows . " autocomplete options.";
+		?>
 
-		<!-- One -->
-			<section id="one" class="wrapper style2">
-				<div class="inner">
-					<div class="grid-style">
+		<!--Make sure the form has the autocomplete function switched off:-->
+		<form autocomplete="off" action="part_details.php" method="get" enctype="multipart/form-data">
+			<input type="text" id="partNumber" name="partNumber" placeholder="Part number">
+			<input type="submit" value="Search" class="button alt">
+		</form>
 
-						<div> <!-- First box in grid -->
-							<div class="box">
-								<div class="content">
-									<header class="align-center">
-										<p>View part information</p>
-										<h2>Parts</h2>
-									</header>
-									<p>
-										Explore part information, part structures and history.
-										<?php
-											$query = "SELECT DISTINCT Number FROM XML_demo.Parts";
-											$query = "SELECT DISTINCT Number FROM XML_demo.Parts WHERE Number LIKE '32895%'";
-											$result = $conn->query($query);
-											echo "<br>Option list contains " . $result->num_rows . " autocomplete options.";
-										?>
-									</p>
+		<!-- Autocomplete -->
+		<script src="scripts/autoComplete.js"></script>
+		<script>
+			<?php
+				$query = "SELECT DISTINCT Number FROM XML_demo.Parts ORDER BY Number";
+				$result = $conn->query($query);
+				$numPart=0; // use counter to avoid printing comma before first result
 
-									<!--Make sure the form has the autocomplete function switched off:-->
-									<form autocomplete="off" action="part_details.php" method="get" enctype="multipart/form-data">
-				            <p><input type="text" id="partNumber" name="partNumber" placeholder="Part number"></p>
-										<footer class="align-center">
-											<input type="submit" value="Search" class="button alt">
-										</footer>
-				          </form>
-
-									<!-- Autocomplete -->
-						      <script src="scripts/autoComplete.js"></script>
-									<script>
-										<?php
-
-											$query = "SELECT DISTINCT Number FROM XML_demo.Parts ORDER BY Number";
-							      	$result = $conn->query($query);
-											$numPart=0; // use counter to avoid printing comma before first result
-
-
-											if ($result->num_rows > 0){
-												// write array var declaration
-												echo 'var parts = [';
-												echo '"option 1"';
+				if ($result->num_rows > 0){
+					// write array var declaration
+					echo 'var parts = [';
+					echo '"option 1"';
 
 //								        while($row = $result->fetch_assoc()){
 //													$numPart++;
@@ -71,30 +40,13 @@
 //								         	echo '"' . $partNumber . '"';
 //								        } // end loop all results
 
-												// write array variable closing
-												echo ', "option 4"';
-												echo "];";
-						      		} // end if num_rows > 0
-											// end adding parts to textfield autocomplete
-										?>
-						      	autocomplete(document.getElementById("partNumber"), parts, 100);
-						      </script>
-
-								</div>
-							</div>
-						</div> <!-- End of first box -->
-
-
-					</div>
-				</div>
-			</section>
-
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-
+											// write array variable closing
+					echo ', "option 4"';
+					echo "];";
+					} // end if num_rows > 0
+					// end adding parts to textfield autocomplete
+			?>
+			autocomplete(document.getElementById("partNumber"), parts, 100);
+		</script>
 	</body>
 </html>
